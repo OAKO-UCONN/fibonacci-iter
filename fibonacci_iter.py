0090d311745ee3f5cb6b438ce5de9f5fd5b5524a
OAKO-UCONN <63368794+OAKO-UCONN@users.noreply.github.com>
2021-06-09 20:34:20 -0400

    Update fibonacci_iter.py

--- fibonacci_iter.py
+++ fibonacci_iter.py
@@ -5,4 +5,12 @@ fibn = 0
 print ('iteration 0: 0')
 for i in range(n-1):
     fibn = prev + prev_prev
-    prev_prev = 
+    prev_prev = prev
+    prev = fibn
+    
+    print('iteration ', i, 'value :', fibn)
+    
+def fib(n):
+    return fib(n-1)+fib(n-2)
+
+print(fib(5))
